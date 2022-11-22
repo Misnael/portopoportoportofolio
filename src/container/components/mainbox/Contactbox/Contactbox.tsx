8f766b1fd39a4f586a4e9399f2bf9c9ac638888d
Misnael <71625384+Misnael@users.noreply.github.com>
2022-11-22 12:44:50 +0200

    NEj

--- src/container/components/mainbox/Contactbox/Contactbox.tsx
+++ src/container/components/mainbox/Contactbox/Contactbox.tsx
@@ -1,23 +1,20 @@
 import React from 'react'
 import styles from './Contactbox.module.css'
 
+
 const Contactbox = () => {
   return (
     <div className={styles.main}>
 
         <div className={styles.content}>
           <div className={styles.list}>
-            <li>Instagram</li> <p></p>
+            <li><a href="https://www.instagram.com/m1.ck3/">Instagram</a></li> <p></p>
             <li>Discord</li> <p></p>
             <li>Gmail</li> <p></p>
             <li>Youtube</li> <p></p>
             <li>Twitch</li> <p></p>
           </div>
-        </div>
-        <div className={styles.sidebar}>
-          <div className={styles.insta}>
-            <iframe className={styles.insta} src="https://www.instagram.com/p/CeD4CBGMBD_/?utm_source=ig_web_copy_link" ></iframe>
-          </div>
+        
         </div>
     
     </div>
