3c23de2b957c2d941d2a0c9aac243b27d1b771f1
Misnael <71625384+Misnael@users.noreply.github.com>
2022-11-24 12:21:58 +0200

    Discord

--- src/container/components/Abtcomp/Abtcomp.tsx
+++ src/container/components/Abtcomp/Abtcomp.tsx
@@ -8,7 +8,14 @@ const Abtcomp = () => {
     <>
         <div className={styles.main}>
             <div className={styles.content}>
-                <div className={styles.text}>Test</div>
+                <div className={styles.text}>
+                    <span>I'm a Halfstack developer im decent at CSS and Tailwind but anything with JS and TS is a challenge for me but im still learning</span>
+                    <span>and i'm currently in a vocational school called Optima studying programming.</span>
+                    <p></p>
+                    <span>I love working on computers and building them which i am pretty decent at aswell.</span>
+                    <p></p>
+                    
+                </div>
             </div>
         </div>
     </>
--- src/container/components/mainbox/Contactbox/Contactbox.tsx
+++ src/container/components/mainbox/Contactbox/Contactbox.tsx
@@ -6,20 +6,24 @@ const Contactbox = () => {
   return (
     <div className={styles.main}>
 
-        <div className={styles.around}>
-          
-        </div>
 
         <div className={styles.content}>
             <div className={styles.list}>
               <li><a href="https://www.instagram.com/m1.ck3/">Instagram</a></li> <p></p>
-              <li>Discord</li> <p></p>
-              <li>Gmail</li> <p></p>
+              <li><a href="https://discord.com/channels/@me">Discord</a></li> <p></p>
+              <li>Mikaelkotanen04@gmail.com</li> <p></p>
               <li>Youtube</li> <p></p>
               <li>Twitch</li> <p></p>
             </div>
           </div>
     
+        <div className={styles.around}>
+          <div className={styles.discord}>
+          <iframe src="https://discord.com/widget?id=864283982291664917&theme=dark" width="350" height="500"
+           allowtransparency="true" frameborder="0"
+           sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+          </div>
+        </div>
     </div>
   )
 }
