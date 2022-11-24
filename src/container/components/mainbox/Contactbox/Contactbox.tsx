3d84dd05f087322e58c68c105164aa44f111c035
Misnael <71625384+Misnael@users.noreply.github.com>
2022-11-24 12:23:34 +0200

    test

--- src/container/components/mainbox/Contactbox/Contactbox.tsx
+++ src/container/components/mainbox/Contactbox/Contactbox.tsx
@@ -20,7 +20,7 @@ const Contactbox = () => {
         <div className={styles.around}>
           <div className={styles.discord}>
           <iframe src="https://discord.com/widget?id=864283982291664917&theme=dark" width="350" height="500"
-           allowtransparency="true" frameborder="0"
+           
            sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
           </div>
         </div>
